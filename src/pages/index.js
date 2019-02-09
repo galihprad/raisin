55af6b46cb5a756212c575167fd013785c82524a
Galih Pradipto Wisnujati <galih.p.w.j@gmail.com>
2019-02-09 16:43:30 +0700

    Update index.js
    lates project belum jadi

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,14 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import ContactUs from '../components/contactUs'
 import Testimoni from '../components/Testimoni'
-import LatesProject from '../components/latesProject'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
     <Link to="/page-2/">Go to page 2</Link>
-    <LatesProject />
+   
     <Testimoni />
     <ContactUs />
   </Layout>
